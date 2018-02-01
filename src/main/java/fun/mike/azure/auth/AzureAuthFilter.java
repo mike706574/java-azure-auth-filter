f927a7506c8eaf0b0c508a3d55485bcf0d7abd72
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-31 23:49:13 -0600

    Use azure-auth-alpha.

--- src/main/java/fun/mike/azure/auth/AzureAuthFilter.java
+++ src/main/java/fun/mike/azure/auth/AzureAuthFilter.java
@@ -1,11 +1,6 @@
 package fun.mike.azure.auth;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotAuthorizedException;
@@ -15,170 +10,29 @@ import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.ext.Provider;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.jwk.source.RemoteJWKSet;
-import com.nimbusds.jose.proc.BadJOSEException;
-import com.nimbusds.jose.proc.JWSKeySelector;
-import com.nimbusds.jose.proc.JWSVerificationKeySelector;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.proc.BadJWTException;
-import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
-import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier;
-import com.nimbusds.jwt.proc.DefaultJWTProcessor;
-
 @Provider
 @PreMatching
 public class AzureAuthFilter implements ContainerRequestFilter {
-    private final String clientId;
     private final String tenantId;
+    private final String clientId;
 
-    private final TypeReference STRING_MAP = new TypeReference<Map<String, Object>>() {};
 
-    public AzureAuthFilter(String clientId, String tenantId) {
-        this.clientId = clientId;
+    public AzureAuthFilter(String tenantId, String clientId) {
         this.tenantId = tenantId;
+        this.clientId = clientId;
     }
 
     public void filter(ContainerRequestContext ctx) throws IOException {
         String header = ctx.getHeaderString(HttpHeaders.AUTHORIZATION);
-        String token = getBearerTokenFromAuthorizationHeader(header);
-        String jwksUrl = getJwksUrl();
 
-        TokenValidationResult result = validateToken(token, jwksUrl);
+        AuthenticationResult result = new Authenticator(tenantId, clientId).authenticate(header);
 
         if (result.failed()) {
-            throw internalServerError(result.getMessage());
+            throw new InternalServerErrorException(result.getMessage());
         }
 
-        if (!result.valid()) {
-            throw unauthorized(result.getMessage());
+        if (result.invalid()) {
+            throw new NotAuthorizedException(result.getMessage());
         }
-    }
-
-    private String getBearerTokenFromAuthorizationHeader(String header) {
-        if (header == null) {
-            throw unauthorized("No \"Authorization\" header present.");
-        }
-
-        List<String> parts = Arrays.asList(header.split(" "));
-
-        if (parts.size() != 2) {
-            throw unauthorized("Malformed \"Authorization\" header.");
-        }
-
-        String scheme = parts.get(0);
-
-        if (!"Bearer".equals(scheme)) {
-            String message = String.format("Unexpected authentication scheme %s in the \"Authorization\" header; expected \"Bearer\".",
-                    scheme);
-            throw unauthorized(message);
-        }
-
-        return parts.get(1);
-    }
-
-    private String getJwksUrl() {
-        URL metadataURL = null;
-
-        String metadataPath = String.format("https://login.microsoftonline.com/%s/v2.0/.well-known/openid-configuration",
-                tenantId);
-        try {
-            metadataURL = new URL(metadataPath);
-        } catch (MalformedURLException ex) {
-            String message = String.format("OpenID provider metadata URL \"%s\" is malformed.",
-                    metadataPath);
-            throw internalServerError(message);
-        }
-
-        Map<String, Object> metadata = null;
-        try {
-            metadata = new ObjectMapper().readValue(metadataURL, STRING_MAP);
-        } catch (IOException ex) {
-            String message = String.format("Failed to parse OpenID provider metadata from \"%s\".",
-                    metadataURL);
-            throw internalServerError(message);
-        }
-
-        if (metadata.containsKey("jwks_uri")) {
-            return (String) metadata.get("jwks_uri");
-        }
-
-
-        String message = String.format("No jwks_uri property present in OpenID provider metadata retrieved from \"%s\".",
-                                       metadataPath);
-        throw internalServerError(message);
-    }
-
-    private TokenValidationResult validateToken(String token, String jwksUrl) {
-        JWKSource<SecurityContext> jwksSource = null;
-        try {
-            jwksSource = new RemoteJWKSet(new URL(jwksUrl));
-        } catch (MalformedURLException ex) {
-            String message = String.format("JWKS URL \"%s\" retrieved from OpenID provider is malformed.",
-                    jwksUrl);
-            return TokenValidationResult.failed(message);
-        }
-
-        return validateToken(token, jwksSource);
-    }
-
-    private TokenValidationResult validateToken(String token, JWKSource<SecurityContext> jwksSource) {
-        ConfigurableJWTProcessor jwtProcessor = new DefaultJWTProcessor();
-
-        JWSAlgorithm expectedJWSAlg = JWSAlgorithm.RS256;
-
-        JWSKeySelector keySelector = new JWSVerificationKeySelector(expectedJWSAlg, jwksSource);
-        jwtProcessor.setJWSKeySelector(keySelector);
-
-        jwtProcessor.setJWTClaimsSetVerifier(new DefaultJWTClaimsVerifier() {
-            @Override
-            public void verify(JWTClaimsSet claimsSet)
-                    throws BadJWTException {
-
-                super.verify(claimsSet);
-
-                if (claimsSet.getExpirationTime() == null) {
-                    throw new BadJWTException("Missing required token expiration claim.");
-                }
-
-                String subject = claimsSet.getSubject();
-                if (clientId.equals(claimsSet.getSubject())) {
-                    String message = String.format("Expected subject \"%s\" to be \"%s\".",
-                            clientId,
-                            subject);
-                    throw new BadJWTException(message);
-                }
-
-                String expectedIssuer = String.format("https://sts.windows.net/%s/",
-                        tenantId);
-
-                if (!expectedIssuer.equals(claimsSet.getIssuer())) {
-                    String message = String.format("Expected issuer \"%s\" to be \"%s\".",
-                            clientId,
-                            expectedIssuer);
-                    throw new BadJWTException(message);
-                }
-            }
-        });
-
-        try {
-            JWTClaimsSet claimsSet = jwtProcessor.process(token, null);
-            return TokenValidationResult.valid(claimsSet.getClaims());
-        } catch (ParseException | JOSEException | BadJOSEException ex) {
-            return TokenValidationResult.invalid(ex.getMessage());
-        }
-    }
-
-    private InternalServerErrorException internalServerError(String message) {
-        return new InternalServerErrorException(message);
-    }
-
-    private NotAuthorizedException unauthorized(String message) {
-        return new NotAuthorizedException(message);
     }
 }
