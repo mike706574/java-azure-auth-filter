3347bf71b8a2548923cae3e3df3284656c9bfa30
EAS7510 <michaeleaster@northwesternmutual.com>
2018-04-30 15:49:43 -0500

    Configurable JWKS retrieval timeouts.

--- src/main/java/fun/mike/azure/auth/alpha/AzureAuthFilterFactory.java
+++ src/main/java/fun/mike/azure/auth/alpha/AzureAuthFilterFactory.java
@@ -1,13 +1,13 @@
 package fun.mike.azure.auth.alpha;
 
 public class AzureAuthFilterFactory {
-    public static AzureAuthFilter simple(String tenantId, String clientId) {
-        Authenticator authenticator = AuthenticatorFactory.build(tenantId, clientId);
+    public static AzureAuthFilter simple(String tenantId, String clientId, int jwksConnectTimeout, int jwksReadTimeout) {
+        Authenticator authenticator = AuthenticatorFactory.build(tenantId, clientId, jwksConnectTimeout, jwksReadTimeout);
         return new AzureAuthFilter(authenticator);
     }
 
-    public static AzureAuthFilter withPathPattern(String tenantId, String clientId, String pathPattern) {
-        Authenticator authenticator = AuthenticatorFactory.build(tenantId, clientId);
+    public static AzureAuthFilter withPathPattern(String tenantId, String clientId, int jwksConnectTimeout, int jwksReadTimeout, String pathPattern) {
+        Authenticator authenticator = AuthenticatorFactory.build(tenantId, clientId, jwksConnectTimeout, jwksReadTimeout);
         return new AzureAuthFilter(authenticator, pathPattern);
     }
 }
